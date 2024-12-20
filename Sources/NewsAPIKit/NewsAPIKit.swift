eaf8940ec1a8fda38d267950890b60ed08f9035c
jyotipatil2505 <jyoti.mitkar55@gmail.com>
2024-12-20 14:22:32 +0530

    Added NewsAPIKit

--- Sources/NewsAPIKit/NewsAPIKit.swift
+++ Sources/NewsAPIKit/NewsAPIKit.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+struct NewsAPIKit {
+    
+    public init() {
+        
+    }
+    
+    func fetchNews() {
+        print("fetchNews called")
+    }
+}
